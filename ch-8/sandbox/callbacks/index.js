f8dad7436acb6cc862a958db742212b56aca434a
Nigel Gomes <nigelgomesdeveloper@gmail.com>
2020-08-08 17:25:16 -0500

    # callbacks & fastseries samples added...

--- ch-8/sandbox/callbacks/index.js
+++ ch-8/sandbox/callbacks/index.js
@@ -1,4 +1,5 @@
 const { readFile} = require('fs')
+const series = require('fastseries')()
 
 // readFile(__filename, (err, contents) => {
 //   if (err) console.error(err)
@@ -6,18 +7,20 @@ const { readFile} = require('fs')
 //   console.log(contents.toString().length)
 // })
 
-const printLength = (err, contents) => {
-  if (err)
-    return console.error(err)
+const printLength = (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
 
-  console.log(contents.toString().length)
+  console.log(Buffer.concat(data).toString().length)
 }
 
-const getLength = (err, contents) => {
+const getLength = (err, data) => {
   if (err)
     return console.error(err)
 
-  return contents.toString().length
+  return data.toString().length
 }
 
 const smallFile = './data/small.html',
@@ -48,27 +51,61 @@ const smallFile = './data/small.html',
 //   })
 // })
 
-// return when all are loaded
-const data = []
-readFile(bigFile, (err, contents) => {
-  console.log('processing bigFile...')
+// // return when all are loaded
+// const data = []
+// readFile(bigFile, (err, contents) => {
+//   console.log('processing bigFile...')
 
-  if (err) console.error(err)
-  else data.push(getLength(err, contents))
+//   if (err) console.error(err)
+//   else data.push(getLength(err, contents))
 
-  readFile(mediumFile, (err, contents) => {
-    console.log('processing mediumFile...')
+//   readFile(mediumFile, (err, contents) => {
+//     console.log('processing mediumFile...')
 
-    if (err) console.error(err)
-    else data.push(getLength(err, contents))
+//     if (err) console.error(err)
+//     else data.push(getLength(err, contents))
 
-    readFile(smallFile, (err, contents) => {
-      console.log('processing smallFile...')
+//     readFile(smallFile, (err, contents) => {
+//       console.log('processing smallFile...')
 
-      if (err) console.error(err)
-      else data.push(getLength(err, contents))
+//       if (err) console.error(err)
+//       else data.push(getLength(err, contents))
 
-      console.log('data', data)
+//       console.log('data', data)
+//     })
+//   })
+// })
+
+// // read variable number of files
+// const files = [smallFile, mediumFile, bigFile]
+// const data = []
+// let index = 0
+// const readRecursive = fileIndex => {
+//   const file = files[fileIndex]
+//   console.log(`processing ${file}...`)
+//   readFile(file, (err, contents) => {
+//     if (err) console.warn(`error: ${file} ${err}`)
+//     else data.push([file, contents.toString().length])
+//     index++
+//     if (index < files.length) readRecursive(index)
+//     else console.log(`data: ${data}`)
+//   })
+// }
+// readRecursive(index)
+
+// using fastseries with halt
+const files = [smallFile, mediumFile, bigFile]
+const readers = files.map(file => {
+  return (_, cb) => {
+    readFile(file, (err, contents) => {
+      console.log(`reading file ${file}`)
+      if (err) {
+        console.warn(err)
+        cb(null, Buffer.alloc(0))
+      }
+      else cb(null, contents)
     })
-  })
+  }
 })
+series(null, readers, null, printLength)
+
