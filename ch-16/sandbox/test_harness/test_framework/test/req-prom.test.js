c5953a24140d6feea0a63efbd892460bf0e59482
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-09-25 09:16:17 -0500

    # jest req-prom added...

---
+++ ch-16/sandbox/test_harness/test_framework/test/req-prom.test.js
@@ -0,0 +1,16 @@
+'use strict'
+
+global.setTimeout = require('timers').setTimeout
+const reqPromise = require('../req-prom')
+
+test('handles neetwork errors', async () => {
+  await expect(reqPromise('http://error.com'))
+          .rejects
+          .toStrictEqual(Error('network error'))
+})
+
+test('responds with data', async () => {
+  const data = await reqPromise('http://ok.com')
+  expect(Buffer.isBuffer(data)).toBeTruthy()
+  expect(data).toStrictEqual(Buffer.from('ok'))
+})
