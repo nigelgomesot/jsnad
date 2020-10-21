ed00b3b682e8f2d2d6a75efd5a7859216911f107
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-10-21 09:35:34 -0500

    # limited parallel WIP...

--- heynode/control_flow/example6.js
+++ heynode/control_flow/example6.js
@@ -29,4 +29,19 @@ const limited = (limit, callbacks, last) => {
   next()
 }
 
+const custom_async = (arg, callback) => {
+  const delay = Math.floor(Math.random() * 5 + 1) * 1000
+  console.log(`custom async with ${arg} & return in ${delay} ms.`)
+  setTimeout(() => {
+    const result = arg * 2
+    console.log(`return with ${arg}, result: ${result}`)
+    callback(result)
+  }, delay)
+}
+
+const final = results => {
+  console.log('results:', results)
+  console.log('done.')
+}
+
 // PENDING
