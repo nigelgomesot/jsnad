f7d684ac8faaa1b589e2ee65d51bd556fcfc4bb2
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-10-26 09:47:51 -0500

    # callback runParallel WIP...

--- heynode/control_flow/rising_stack/callback.js
+++ heynode/control_flow/rising_stack/callback.js
@@ -2,9 +2,7 @@
 // REF: https://blog.risingstack.com/node-js-async-best-practices-avoiding-callback-hell-node-js-at-scale/#avoidingcallbackhellwithcontrolflowmanagers
 
 const { fastFunction, mediumFunction, slowFunction } = require('./helpers')
-
 const results = []
-
 const runSerial = (cb) => {
   fastFunction((err, result) => {
     if (err) return cb(err)
@@ -22,10 +20,55 @@ const runSerial = (cb) => {
   })
 }
 
-console.time('callback')
-runSerial(err => {
+// console.time('callback runSerial')
+// runSerial(err => {
+//   if(err) console.warn('error occurred:', err)
+//   else console.log('done.')
+//   console.log('results:', results)
+//   console.timeEnd('callback runSerial')
+// })
+
+
+// REF: https://www.alxolr.com/articles/learning-asynchronous-programming-in-node-js-with-callbacks#run-in-parallel
+const runParallel = (cb) => {
+  const results = []
+  let pending = 0
+
+  fastFunction((err, result) => {
+    gateKeeper()
+  })
+
+  mediumFunction((err, result) => {
+    gateKeeper()
+  })
+
+  slowFunction((err, result) => {
+    gateKeeper()
+  })
+
+  function gateKeeper() {
+    let order = pending
+    pending++
+    console.log('in gatekeeper')
+
+    return (err, data) => {
+      pending--
+
+      if (err) return done(err)
+
+      results.push(data)
+
+      if (!pending) {
+        return cb(null, results)
+      }
+    }
+  }
+}
+
+console.time('callback runParallel')
+runParallel((err, results) => {
   if(err) console.warn('error occurred:', err)
   else console.log('done.')
   console.log('results:', results)
-  console.timeEnd('callback')
+  console.timeEnd('callback runParallel')
 })
