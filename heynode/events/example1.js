ba2ac8ea4f87b9a1f986a948befeb7aef0c94115
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-11-25 09:21:49 -0600

    # exmaple1 added...

---
+++ heynode/events/example1.js
@@ -0,0 +1,13 @@
+
+// REF: https://www.freecodecamp.org/news/how-to-code-your-own-event-emitter-in-node-js-a-step-by-step-guide-e13b7e7908e1/
+
+const EventEmitter = require('events')
+const myEmitter = new EventEmitter()
+
+const cb1 = () => console.log('cb1 invoked')
+const cb2 = () => console.log('cb2 invoked')
+
+myEmitter.on('event1', cb1)
+myEmitter.on('event1', cb2)
+
+myEmitter.emit('event1')
