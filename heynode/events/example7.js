32f7048a4f8dfbb1f57c0ec845d9adb80e4fe101
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-11-30 10:22:54 -0600

    # async withTime added...

--- heynode/events/example7.js
+++ heynode/events/example7.js
@@ -1,10 +1,11 @@
 // async demo
 
 const EventEmitter = require('events')
+const fetch = require('node-fetch')
 
 class WithTime extends EventEmitter {
   execute(asyncFn, ...args) {
-    this.emit('begin')
+    this.emit('start')
     console.time('execute')
 
     this.on('data', data => console.log('data received:', data))
@@ -15,7 +16,20 @@ class WithTime extends EventEmitter {
 
       this.emit('data', data)
       console.timeEnd('execute')
-      this.emit('end')
+      this.emit('stop')
     })
   }
 }
+
+const withTime = new WithTime()
+
+withTime.on('start', () => console.log('withTime started'))
+withTime.on('stop', () => console.log('withTime stopped.'))
+
+const readFile = (url, cb) => {
+  fetch(url)
+    .then(resp => resp.json())
+    .then(data => cb(null, data))
+}
+
+withTime.execute(readFile, 'https://jsonplaceholder.typicode.com/posts/1')
