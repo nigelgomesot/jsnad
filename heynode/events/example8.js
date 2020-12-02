fc92fcf9a33772c0b43eb8254e13f70b953ea1be
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-12-02 08:49:09 -0600

    # custom event emitter WIP...

--- heynode/events/example8.js
+++ heynode/events/example8.js
@@ -7,6 +7,32 @@ class CustomEventEmitter {
   addListener(event, fn) {
     this.listeners[event] = this.listeners[event] || []
     this.listeners[event].push(fn)
+
+    return this
+  }
+
+  on(event, fn) {
+    this.addListener(event, fn)
+  }
+
+  removeListener(event, fn) {
+    const eventListeners = this.listeners[event]
+
+    if (!eventListeners)
+      return this
+
+    for (let i = 0; i < eventListeners.length; i++) {
+      if (eventListeners[i] === fn) {
+        eventListeners.splice(i, 1)
+        break
+      }
+    }
+
+    return this
+  }
+
+  off(event, fn) {
+    this.removeListener(event, fn)
   }
 }
 
@@ -14,16 +40,38 @@ let customEventEmitter,
     expectedResult,
     result
 
+// test on
 const fn1 = () => console.log('fn1 invoked')
 customEventEmitter = new CustomEventEmitter()
-customEventEmitter.addListener('event1', fn1)
+customEventEmitter.on('event1', fn1)
 result = customEventEmitter.listeners
 expectedResult = { 'event1': [fn1] }
 assert.deepEqual(result, expectedResult)
 
-customEventEmitter.addListener('event1', fn1)
+customEventEmitter.on('event1', fn1)
 result = customEventEmitter.listeners
 expectedResult = { 'event1': [fn1, fn1] }
+assert.deepEqual(result, expectedResult)
+
+// test off
+const fn2 = () => console.log('fn2 invoked')
+customEventEmitter.on('event2', fn2)
+result = customEventEmitter.listeners
+expectedResult = { 'event1': [fn1, fn1], 'event2': [fn2] }
+assert.deepEqual(result, expectedResult)
+customEventEmitter.off('event1', fn1)
+result = customEventEmitter.listeners
+expectedResult = { 'event1': [fn1], 'event2': [fn2] }
+assert.deepEqual(result, expectedResult)
+
+customEventEmitter.off('event1', fn1)
+result = customEventEmitter.listeners
+expectedResult = { 'event1': [], 'event2': [fn2] }
+assert.deepEqual(result, expectedResult)
+
+customEventEmitter.off('event1', fn1)
+result = customEventEmitter.listeners
+expectedResult = { 'event1': [], 'event2': [fn2] }
 assert.deepEqual(result, expectedResult)
 
 console.log('done')
