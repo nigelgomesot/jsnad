35a5711afe777945ed5291e5a64bc11d345117b7
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-12-11 11:36:01 -0600

    # Promise errors WIP...

--- heynode/error_handling/example2.js
+++ heynode/error_handling/example2.js
@@ -1,6 +1,8 @@
 // REF: https://www.valentinog.com/blog/error/#what-is-an-exception
 
-// Error handlling in promises
+// Error handling in promises
+
+// basic async
 const failAfterSeconds = (ms) => {
   return new Promise((_, reject) => {
     setTimeout(() => {
@@ -9,8 +11,37 @@ const failAfterSeconds = (ms) => {
   })
 }
 
-failAfterSeconds(3000)
+const runFailAfterSeconds = () => {
+  failAfterSeconds(3000)
   .catch(err => {
     console.warn('error message:', err.message)
     console.warn('error stack:', err.stack)
   })
+}
+//runFailAfterSeconds()
+
+const resolvedPromise = Promise.resolve('resolved')
+const rejectedPromise = Promise.reject('rejected')
+
+// Promise.all
+const runPromiseAll = () => {
+  Promise.all([resolvedPromise, rejectedPromise])
+    .then(results => console.log('results', results))
+    .catch(err => console.warn('error:', err))
+    .finally(() => console.log('Promise.all done.'))
+}
+//runPromiseAll()
+
+// Promise.race
+const runPromiseRace = () => {
+  Promise.race([resolvedPromise, rejectedPromise])
+    .then(results => console.log('results', results))
+    .catch(err => console.warn('error:', err))
+    .finally(() => console.log('Promise.race done.'))
+
+  Promise.race([rejectedPromise, resolvedPromise])
+    .then(results => console.log('results', results))
+    .catch(err => console.warn('error:', err))
+    .finally(() => console.log('Promise.race done.'))
+}
+runPromiseRace()
