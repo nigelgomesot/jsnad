4722c7442aef494da164157b4746208102eda1bf
Nigel Gomes <nigelgomes.ot@gmail.com>
2020-12-18 09:08:16 -0600

    # callback error handling in node added...

---
+++ heynode/error_handling/example6.js
@@ -0,0 +1,22 @@
+// REF: https://www.valentinog.com/blog/error/#asynchronous-error-handling-in-nodejs-the-callback-pattern
+
+// Error handling via callback in Node.js
+const { readFile } = require('fs')
+
+const errorHandler = (error) => {
+  console.error('error occurred:', error.message)
+}
+const dataHandler = (data) => {
+  console.log('data:', data)
+}
+
+const readFileData = (path) => {
+  readFile(path, { encoding: 'utf-8'}, (error, data) => {
+    if (error)
+      return errorHandler(error)
+    else
+      return dataHandler(data)
+  })
+}
+readFileData('invalid/path')
+readFileData('example6.js')
