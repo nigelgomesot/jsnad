7aae003389be252d6521ee2753897a7d92b0aebb
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-04 12:56:16 -0600

    # modules WIP...

---
+++ heynode/modules/example1.js
@@ -0,0 +1,12 @@
+// Export via module.exports
+
+const math = require('./example1_module')
+
+const sum = math.add(1, 2)
+console.log('sum (1,2):', sum)
+
+const diff = math.subtract(3, 1)
+console.log('diff (3, 1):', diff)
+
+const double = math.double(3)
+console.log('double (3):', double)
---
+++ heynode/modules/example1_module.js
@@ -0,0 +1,19 @@
+const add = (num1, num2) => {
+  return num1 + num2
+}
+
+const subtract = (num1, num2) => {
+  return num1 - num2
+}
+
+const double = (num) => {
+  return num * 2
+}
+
+module.exports ={
+  add,
+  subtract,
+  double
+}
+
+
