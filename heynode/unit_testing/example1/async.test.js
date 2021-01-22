87d3fec1f940a2f4504ccc0bd45ee60d6437d1d7
Nigel Gomes <nigelgomes.ot@gmail.com>
2021-01-22 12:47:41 -0600

    # jest WIP...

--- heynode/unit_testing/example1/async.test.js
+++ heynode/unit_testing/example1/async.test.js
@@ -34,3 +34,21 @@ describe('testing promises', () => {
     })
   })
 })
+
+// Async/Await
+describe('testing async/await', () => {
+  const upperCasePromise = str => {
+    return new Promise((resolve, reject) => {
+      if (!str) {
+        return('empty string')
+      }
+
+      resolve(str.toUpperCase())
+    })
+  }
+
+  it('converts str to upper case via async/await', async () => {
+    const str = await upperCasePromise('test')
+    expect(str).toBe('TEST')
+  })
+})
